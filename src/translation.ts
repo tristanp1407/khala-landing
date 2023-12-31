a2171ee57a558ec38d0244ce5185946b2f8e2875
Tristan Plet <tristan.plet@octopusev.com>
2023-12-30 19:10:50 -0600

    Fix errros

--- src/translation.ts
+++ src/translation.ts
@@ -1,17 +1,23 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import XHR from "i18next-xhr-backend";
+import HttpBackend from "i18next-http-backend"; // Import the new HTTP backend
 
 import translationEn from "./locales/en/translation.json";
 import translationEs from "./locales/es/translation.json";
 
 i18n
-  .use(XHR)
+  .use(HttpBackend) // Use HttpBackend instead of XHR
   .use(LanguageDetector)
   .init({
     debug: false,
     lng: "en",
     fallbackLng: "en",
+
+    // Add backend configuration
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json", // Path to your locale files
+      // other options like crossDomain might be needed depending on your setup
+    },
 
     keySeparator: false,
     interpolation: {
